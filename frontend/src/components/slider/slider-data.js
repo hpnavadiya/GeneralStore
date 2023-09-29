80d3e384b021ded60e28a35034048eaa4a201c87
hpnavadiya <navadiyaharsh8@gmail.com>
2023-09-29 16:42:06 -0400

    Start working on slider on home page

---
+++ frontend/src/components/slider/slider-data.js
@@ -0,0 +1,27 @@
+export const sliderData = [
+    {
+        image: "https://i.ibb.co/D1TvpVL/white-shoe.jpg",
+        heading: "Shoes Villa",
+        desc: "Up to 30% off",
+    },
+    {
+        image: "https://i.ibb.co/D1TvpVL/white-shoe.jpg",
+        heading: "Shoes Villa",
+        desc: "Up to 30% off",
+    },
+    {
+        image: "https://i.ibb.co/D1TvpVL/white-shoe.jpg",
+        heading: "Shoes Villa",
+        desc: "Up to 30% off",
+    },
+    {
+        image: "https://i.ibb.co/D1TvpVL/white-shoe.jpg",
+        heading: "Shoes Villa",
+        desc: "Up to 30% off",
+    },
+    {
+        image: "https://i.ibb.co/D1TvpVL/white-shoe.jpg",
+        heading: "Shoes Villa",
+        desc: "Up to 30% off",
+    },
+];
