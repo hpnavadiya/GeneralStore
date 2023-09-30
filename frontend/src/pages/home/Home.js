4c00a45a79c093e001199d3cb23bc6ea8aa1f8e1
hpnavadiya <navadiyaharsh8@gmail.com>
2023-09-30 16:09:33 -0400

    Add info boxes in home page

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Slider from '../../components/slider/Slider'
+import "./Home.scss"
+import HomeInfoBox from './HomeInfoBox'
 
 const Home = () => {
   return (
     <>
       <Slider />
+      <section>
+        <div className="container">
+          <HomeInfoBox/>
+        </div>
+      </section>
     </>
   )
 }
