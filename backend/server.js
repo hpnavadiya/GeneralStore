3110460ab0468b4b10874278fac48a4952e733f4
sagarmalhotra1930 <Sagarmalhotra3003@gmail.com>
2023-10-08 21:00:43 -0400

    Update Server file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,40 @@
-console.log("Hello World!!!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware");
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "http://GeneralStore.vercel.app"],
+        credentials: true,
+    })
+);
+
+// Middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000; 
+
+// Mongodb connection
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT} Port`)
+    })
+}).catch((err)=> console.log(err));
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home Page....")
+})
+
+app.use("/api/users", userRoute);
