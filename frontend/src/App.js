8ab2c91dfdfb781ace5178e312040e404dbb316f
hpnavadiya <navadiyaharsh8@gmail.com>
2023-10-14 09:37:59 -0400

    Working on connect register page with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import { Spinner } from "./components/loader/Loader";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+
+  axios.defaults.withCredentials = true
+
   return (
     <>
       <BrowserRouter>
+      <ToastContainer/>
         <Header />
         <Spinner />
         <Routes>
--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -43,3 +43,10 @@ export const shortenText = (text, n) => {
     }
     return 0; // If the _id is not found, return 0 or any default value
   }
+
+   // Validate email
+   export const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
