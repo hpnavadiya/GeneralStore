9e972a86dfb4b6dc41b2f4bb5268b8b937890d85
hpnavadiya <>
2023-11-22 15:57:19 -0500

    Fix bug on backend

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -12,9 +12,11 @@ const initialState = {
 };
 
 // Register User
+// Handling asynchronous actions in Redux
 export const register = createAsyncThunk(
     "auth/register",
     async (userData, thunkAPI) => {
+        console.log("-------------", userData)
         try {
             return await authService.register(userData)
         } catch (error) {
@@ -26,7 +28,95 @@ export const register = createAsyncThunk(
             return thunkAPI.rejectWithValue(message);
         }
     }
-)
+);
+
+// Login User
+export const login = createAsyncThunk(
+    "auth/login",
+    async (userData, thunkAPI) => {
+        try {
+            return await authService.login(userData)
+        } catch (error) {
+            const message = (
+                error.response &&
+                error.response.data &&
+                error.response.data.message
+            ) || error.message || error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
+// Logout User
+export const logout = createAsyncThunk(
+    "auth/logout",
+    async (_, thunkAPI) => {
+        try {
+            return await authService.logout()
+        } catch (error) {
+            const message = (
+                error.response &&
+                error.response.data &&
+                error.response.data.message
+            ) || error.message || error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
+// Get Login Status of User
+export const getLoginStatus = createAsyncThunk(
+    "auth/getLoginStatus",
+    async (_, thunkAPI) => {
+        try {
+            return await authService.getLoginStatus()
+        } catch (error) {
+            const message = (
+                error.response &&
+                error.response.data &&
+                error.response.data.message
+            ) || error.message || error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
+// Get User
+export const getUser = createAsyncThunk(
+    "auth/getUser",
+    async (_, thunkAPI) => {
+        try {
+            return await authService.getUser()
+        } catch (error) {
+            const message = (
+                error.response &&
+                error.response.data &&
+                error.response.data.message
+            ) || error.message || error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
+// Update User
+export const updateUser = createAsyncThunk(
+    "auth/updateUser",
+    async (userData, thunkAPI) => {
+        try {
+            return await authService.updateUser(userData)
+        } catch (error) {
+            const message = (
+                error.response &&
+                error.response.data &&
+                error.response.data.message
+            ) || error.message || error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
+
+// Handle response
 
 const authSlice = createSlice({
     name: "auth",
@@ -112,8 +202,43 @@ const authSlice = createSlice({
                 state.isError = true;
                 state.message = action.payload;
             })
-;
+            // Get User Details
+            .addCase(getUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.isLoggedIn = true;
+                state.user = action.payload;
+            })
+            .addCase(getUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+                toast.error(action.payload);
+            })
+            // Update User
+            .addCase(updateUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(updateUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.isLoggedIn = true;
+                state.user = action.payload;
+                toast.success("User Updated");
+            })
+            .addCase(updateUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+                toast.error(action.payload);
+            });
     }
 });
 
-export const { } = authSlice.actions;
+// register user
+export const { RESET_AUTH } = authSlice.actions;
+
+export default authSlice.reducer;
