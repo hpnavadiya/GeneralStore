85c1bb8cd5991bd71d5798a5a6b8ef018e637315
hpnavadiya <>
2023-11-24 14:52:50 -0500

    Implement backend of Payment Gateway integration

---
+++ backend/controllers/productController.js
@@ -0,0 +1,86 @@
+const productModel = require("../models/productModel");
+const categoryModel = require("../models/categoryModel.js");
+const orderModel = require("../models/orderModel.js");
+const asyncHandler = require("express-async-handler");
+const fs = require("fs");
+const slugify = require("slugify");
+
+const braintree = require("braintree");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//payment gateway
+var gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    merchantId: process.env.BRAINTREE_MERCHANT_ID,
+    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+    privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
+
+//payment gateway api
+//token
+const braintreeTokenController = async (req, res) => {
+    try {
+        gateway.clientToken.generate({}, function (err, response) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.send(response);
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//payment
+const brainTreePaymentController = async (req, res) => {
+    try {
+        const { nonce, cart } = req.body;
+        let total = 0;
+        cart.map((i) => {
+            total += i.price;
+        });
+        let newTransaction = gateway.transaction.sale(
+            {
+                amount: total,
+                paymentMethodNonce: nonce,
+                options: {
+                    submitForSettlement: true,
+                },
+            },
+            function (error, result) {
+                if (result) {
+                    const order = new orderModel({
+                        products: cart,
+                        payment: result,
+                        buyer: req.user._id,
+                    }).save();
+                    res.json({ ok: true });
+                } else {
+                    res.status(500).send(error);
+                }
+            }
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = {
+    brainTreePaymentController,
+    braintreeTokenController,
+    createProductController,
+    deleteProductController,
+    getProductController,
+    getSingleProductController,
+    productCategoryController,
+    productCountController,
+    productFiltersController,
+    productListController,
+    productPhotoController,
+    realtedProductController,
+    searchProductController,
+    updateProductController,
+}
