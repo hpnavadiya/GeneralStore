39ee2e7afd2be22f391026c44a5d019ef0dbefcb
Keval Akbari <65550224+kevalakbari@users.noreply.github.com>
2023-11-25 00:49:30 -0500

    Cretae model to store order histroy

---
+++ backend/models/orderModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        type: mongoose.ObjectId,
+        ref: "Products",
+      },
+    ],
+    payment: {},
+    buyer: {
+      type: mongoose.ObjectId,
+      ref: "users",
+    },
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "deliverd", "cancel"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
