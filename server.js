aa3e6a6f64b55777033ca7529dc08364a379547b
kevalakbari <kevalakbari4242@gmail.com>
2023-12-14 21:17:23 -0500

    Fix bug for host app

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import { fileURLToPath } from "url";
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+// ES6
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
