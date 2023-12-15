b302ef50d0f8e6a7fb89d1621062a19a5402efb9
hpnavadiya <navadiyaharsh8@gmail.com>
2023-12-14 19:47:05 -0500

    Implement SEO

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -5,7 +5,15 @@ import { Helmet } from "react-helmet";
 import { Toaster } from "react-hot-toast";
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
-    <div>
+
+    <div div >
+      {/* react-helmet provides a convenient way to manage and update these meta tags and other 
+head elements within your React components. Instead of directly manipulating the DOM or 
+using traditional methods to update the head, you can use 
+react-helmet to define these changes in a more declarative and component-based manner. */}
+
+
+      {/* For SEO Implementation we use REACT HELMET  */}
       <Helmet>
         <meta charSet="utf-8" />
         <meta name="description" content={description} />
@@ -24,11 +32,12 @@ const Layout = ({ children, title, description, keywords, author }) => {
   );
 };
 
+// For SEO 
 Layout.defaultProps = {
   title: "Ecommerce app - General Store",
-  description: "mern stack project",
-  keywords: "mern,react,node,mongodb",
-  author: "Techinfoyt",
+  description: "MERN stack Web Project",
+  keywords: "Mern, React, Node, Mongodb",
+  author: "Group8 - Capstone",
 };
 
 export default Layout;
