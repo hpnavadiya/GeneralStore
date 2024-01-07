6e53ab6f3c8cb7a9cacdfc8a774a64e5ba5502b2
hpnavadiya <140366191+hpnavadiya@users.noreply.github.com>
2024-01-06 21:28:15 -0500

    Fix bugs

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-
+import path from 'path';
+import { fileURLToPath } from "url";
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+// ES6
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -21,6 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -28,9 +34,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
+app.use("*", function(req, res){
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
